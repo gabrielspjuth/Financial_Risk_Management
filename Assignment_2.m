ddb68368e82adf17963eaa0ff6b596befbb321f4
gabrielspjuth <152022936+gabrielspjuth@users.noreply.github.com>
2023-12-03 23:55:25 +0100

    Update Assignment_2.m

--- Assignment_2.m
+++ Assignment_2.m
@@ -9,15 +9,15 @@
 % Task 1: VaR and Expected Shortfall
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% Load the data from the file 'data.txt'.
 [S, headers, raw] = xlsread("timeSeries.xlsx");
 
 portfolio_value = 10;
 
 n = 15; % There are 15 different stocks in the portfolio
 omega = 1 / n;
+omega_vector = ones(15, 1) * omega;
 
-t = 1/52; % Time of observed data
+t = 1/52; % Time of observed data. Assuming 52 trading weeks per year.
 confidence_level_95 = 0.95;
 confidence_level_975 = 0.975;
 confidence_level_99 = 0.99;
@@ -29,66 +29,21 @@ stocks = 15;
 
 %% Subtask 1 (a): Calculation of Value at Risk
 
+% Calculating returns matrix for all stocks from t = 1 ... T + 1
 for j = 1:stocks
     for i = prices:-1:2
         R(i-1, j) = ((S((i-1), j+1)) - S(i, j+1)) / S(i, j+1);
     end
 end
 
+C = cov(R);
 
+portfolio_volatiliy = sqrt(transpose(omega_vector) * C * omega_vector);
 
+VaR_95 = norminv(confidence_level_95) * portfolio_volatiliy * portfolio_value;
 
+Var_975 = norminv(confidence_level_975) * portfolio_volatiliy * portfolio_value;
 
+VaR_99 = norminv(confidence_level_99) * portfolio_volatiliy * portfolio_value;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-%% Subtask 1.2: Clean Data
-% Remove any NaN values from the data.
-cleaned_data = cleanData(data);
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Task 2: Model Training
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%% Subtask 2.1: Split Data
-% Split the data into training and testing sets.
-[train_data, test_data] = splitData(cleaned_data);
-
-%% Subtask 2.2: Train Model
-% Train a machine learning model using the training data.
-model = trainModel(train_data);
-
-%% Subtask 2.3: Evaluate Model
-% Evaluate the model using the testing data.
-accuracy = evaluateModel(model, test_data);
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Task 3: Results Analysis
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%% Subtask 3.1: Display Results
-% Display the accuracy of the trained model.
-disp(['Model Accuracy: ' num2str(accuracy)]);
-
-%% Subtask 3.2: Visualize Results
-% Visualize the results using plots or other visualization techniques.
-visualizeResults(model, test_data);
+%% Subtask 1 (b): Calculation of Relative VaR
